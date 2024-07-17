1fb29871885e73fb171a3ce06cf51917b036b4e6
AThings <2846965655@qq.com>
2024-07-17 15:39:05 +0800

    build: 给src文件夹起一个别名

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 // 引用element 插件和样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
